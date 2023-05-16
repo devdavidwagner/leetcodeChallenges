528084902af0486e78c77e6e08aef8f3bf69e006
David Wagner <david.karl.wagner@live.com>
2023-05-16 16:27:50 -0400

    Made all menus loopable

--- Program.cs
+++ Program.cs
@@ -3,37 +3,60 @@
 class Program
 {
     static void Main(string[] args)
-    {   
-        Console.WriteLine("\nWelcome to David's Leetcode Challenge Menu!\n Select an app (number key):\n");
-        Console.WriteLine("1. Roman Numerals to Integers\n");
-        Console.WriteLine("2. Two Sum Solution\n");
-        Console.WriteLine("3. Reverse Integer Algorithm\n");
-        ConsoleKeyInfo keyInfo = Console.ReadKey();
-
-        switch(keyInfo.Key) {
-                case ConsoleKey.D1:
-                    RomanToIntMain();
-                    break;
-                case ConsoleKey.D2:
-                    TwoSum();
-                    break;
-                case ConsoleKey.D3:
-                    ReverseInt();
-                    break;
-                default:
-                    break;
+    {
+        bool mainRunning = true;   
+        while(mainRunning){
+            Console.WriteLine("\nWelcome to David's Leetcode Challenge Menu!\n Select an app (number key) - Press escape to exit:\n");
+            Console.WriteLine("1. Roman Numerals to Integers");
+            Console.WriteLine("2. Two Sum Solution");
+            Console.WriteLine("3. Reverse Integer Algorithm");
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+        
+            switch(keyInfo.Key) {
+                    case ConsoleKey.D1:
+                        RomanToIntMain();
+                        break;
+                    case ConsoleKey.D2:
+                        TwoSum();
+                        break;
+                    case ConsoleKey.D3:
+                        ReverseInt();
+                        break;
+                    case ConsoleKey.Escape:
+                        mainRunning = false;
+                        break;
+                    default:
+                        mainRunning = false;
+                        break;
+            }
         }
+  
     }
 
     static void ReverseInt() {
-        Console.WriteLine("\nWelcome to David's Reverse Integer Solution!\n");
-        Console.WriteLine("\nPlease enter an integer:");
-        string value = "";
-        value = Console.ReadLine() ?? throw new Exception("Error! Nothing entered.");
+        Console.WriteLine("\nWelcome to David's Reverse Integer Solution!\nType EXIT to go back to main Menu.\n");
 
-        int chosenInt = int.Parse(value);
-        int answer = ReverseAlgo(chosenInt);
-        Console.WriteLine("\nThis is your integer in reverse: " + answer.ToString());
+        bool running = true;
+        while(running){
+            Console.WriteLine("\nPlease enter an integer:");        
+            string value = Console.ReadLine() ?? "";
+            int test = 0;
+            if(value != null && value.ToUpper() == "EXIT"){
+              running = false;
+              continue;
+            }
+            if(value == "" || value == null || !int.TryParse(value, out test)){
+                Console.WriteLine("No integer entered.");
+                continue;
+            }      
+            else if (value != "" && value != null){               
+                int chosenInt = int.Parse(value);
+                int answer = ReverseAlgo(chosenInt);
+                Console.WriteLine("\nThis is your integer in reverse: " + answer.ToString());
+            }
+
+        }
+ 
     }
 
     //TODO: MAKE LOOPABLE
@@ -67,32 +90,55 @@ class Program
 
     //TO DO: MAKE LOOPABLE
      static void TwoSum(){
-        Console.WriteLine("\nWelcome to David's Two Sum Solution!\n");
-        Console.WriteLine("\nPlease choose a problem:\n");
+        Console.WriteLine("\nWelcome to David's Two Sum Solution!\nType EXIT to return to menu.\n");
+        bool running = true;
+        
+        while(running){
+            string errorMsg = "";
+            Console.WriteLine("\nPlease enter a Two-Sum problem using the following format:\n");
+            Console.WriteLine("[numbers]=[target]  e.g. 1,2,3=4");
+            string value = Console.ReadLine() ?? "";
+            if(value != null && value.ToUpper() == "EXIT"){
+              running = false;
+            }
+            else if(value != "" && value != null)
+            {
+                string numbers = value.Split("=")[0];
+                string[] numsSplit = numbers.Split(',');
+                string target = value.Split("=")[1];
 
-        Console.WriteLine("1. nums = [2,7,11,15], target = 9\n");
-        Console.WriteLine("2. nums = [3,2,4], target = 6\n");
-        Console.WriteLine("3. nums = [3,2,3], target = 6\n");
-        ConsoleKeyInfo keyInfo = Console.ReadKey();
-        int[] answer;
-        switch(keyInfo.Key) {
-                case ConsoleKey.D1:
-                    answer = TwoSumAlgo(new int[]{2,7,11,15}, 9);
-                    Console.WriteLine($"The solution for #1 is: {answer[0]},{answer[1]}");
-                    break;
-                case ConsoleKey.D2:
-                    answer = TwoSumAlgo(new int[]{3,2,4}, 6);
-                    Console.WriteLine($"The solution for #2 is: {answer[0]},{answer[1]}");
-                    break;
-                case ConsoleKey.D3:
-                    answer = TwoSumAlgo(new int[]{3,2,3}, 6);
-                    Console.WriteLine($"The solution for #3 is: {answer[0]},{answer[1]}");
-                    break;
-                default:
-                    break;
+                int[] toFuncNums = new int[numsSplit.Length];
+                for (int i = 0; i < numsSplit.Length; i++)
+                {
+                    int outTest = 0;
+                    if(int.TryParse(numsSplit[i], out outTest)){
+                        toFuncNums[i] = outTest;
+                    }
+                    else{
+                        errorMsg = $"Value: {numsSplit[i]} is not a valid integer for the input numbers.";
+                    }
+                }
+                int outTestTarget = 0;
+                int toFuncTarget = 0;
+                if(int.TryParse(target, out outTestTarget)){
+                    toFuncTarget = outTestTarget;
+                }
+                else{
+                    errorMsg += $"\nValue: {outTestTarget} is not a valid integer for the target sum.";
+                }
+
+                if(errorMsg != ""){
+                    Console.WriteLine(errorMsg);
+                } 
+                //success
+                else{
+                    int[] answer = TwoSumAlgo(toFuncNums,toFuncTarget);
+                    Console.WriteLine($"The answer to your Two-Sum problem of {value} is : {numsSplit[answer[0]]} + {numsSplit[answer[1]]} = {target}");
+                }
+
+            } 
+          
         }
-
-       
     }
 
     static int[] TwoSumAlgo(int[] nums, int target) {
@@ -115,14 +161,30 @@ class Program
         return sumInd;
     }
 
-    static void RomanToIntMain(){
-        Console.WriteLine("Welcome to David's Roman Numerals to Integer converter!\n Enter a Roman numeral to convert:\n");
-        string value = "";
-        value = Console.ReadLine() ?? throw new Exception("Error! Nothing entered.");
+    static void RomanToIntMain(){     
+        bool running = true;
+        while(running){
+            Console.WriteLine("\nWelcome to David's Roman Numerals to Integer converter!\nType EXIT to return to menu.\nEnter a Roman numeral to convert:\n");
+            string errorMsg = "";
+            string value = "";
+            value = Console.ReadLine() ?? "";
+            if(value == null || value == ""){
+                errorMsg = "No value entered.\n";
+            }
+            else if(value.ToUpper() == "EXIT"){
+                running = false;
+            }
+            else{
+                int returnedInt = RomanToInt(value);
+                Console.WriteLine($"YOUR NUMERAL: {value} EQUALS {returnedInt} in INTEGER");
+            }
 
-        int returnedInt = RomanToInt(value);
+            if(errorMsg != null && errorMsg != ""){
+                Console.WriteLine(errorMsg);
+            }           
 
-        Console.WriteLine($"YOUR NUMERAL: {value} EQUALS {returnedInt} in INTEGER");
+        }
+
     }
     static int RomanToInt(string s) {          
         string[,] romanValuePairs = {{"I", "1"}, {"V", "5"}, {"X", "10"}, {"L", "50"}, {"C", "100"}, {"D", "500"}, {"M", "1000"}};
